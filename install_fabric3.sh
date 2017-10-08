507f9b6760db6ebbd1b49dd64490ec66dcba082b
root <110@holbertonschool.com>
2017-10-08 04:35:06 +0000

    -y automatic yes for prompt

--- install_fabric3.sh
+++ install_fabric3.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 pip3 uninstall Fabric
-sudo apt-get install libffi-dev
-sudo apt-get install libssl-dev
-sudo apt-get install python3.4-dev
-sudo apt-get install libpython3-dev
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y python3.4-dev
+sudo apt-get install -y libpython3-dev
 pip3 install pyparsing
 pip3 install appdirs
 pip3 install Fabric3
