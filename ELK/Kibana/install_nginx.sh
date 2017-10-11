ef765fb02e988f03354e6eb40e15546fbff3daed
110@holbertonschool.com <110@holbertonschool.com>
2017-10-11 01:40:43 +0000

    include configure nginx in setup kibana

--- ELK/Kibana/install_nginx.sh
+++ ELK/Kibana/install_nginx.sh
@@ -9,7 +9,3 @@ sudo apt-get install -y apache2-utils
 # add kibanaadmin user  to nginx
 sudo htpasswd -c /etc/nginx/htpasswd.users kibanaadmin
 
-#configure /etc/nginx/sites-available/default
-# --- run script here
-
-sudo service nginx restart
--- ELK/Kibana/setup_kibana.sh
+++ ELK/Kibana/setup_kibana.sh
@@ -3,3 +3,4 @@
 #install kibana
 /usr/bin/env bash install_kibana.sh
 /usr/bin/env bash install_nginx.sh
+/usr/bin/env bash configure_nginx.sh
