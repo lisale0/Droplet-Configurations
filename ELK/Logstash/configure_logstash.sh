ec17757c2102e5b1995f9a87a7bbf06aeedf7a31
110@holbertonschool.com <110@holbertonschool.com>
2017-10-11 06:00:59 +0000

    add config file

---
+++ ELK/Logstash/configure_logstash.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo 'input {
+  beats {
+    port => 5044
+    ssl => true
+    ssl_certificate => "/etc/pki/tls/certs/logstash-forwarder.crt"
+    ssl_key => "/etc/pki/tls/private/logstash-forwarder.key"
+  }
+}' | sudo tee /etc/logstash/conf.d/02-beats-input.conf
+
+
+echo 'filter {
+  if [type] == "syslog" {
+    grok {
+      match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{DATA:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" }
+      add_field => [ "received_at", "%{@timestamp}" ]
+      add_field => [ "received_from", "%{host}" ]
+    }
+    syslog_pri { }
+    date {
+      match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ]
+    }
+  }
+}' | sudo tee /etc/logstash/conf.d/10-syslog-filter.conf
+
+echo 'output {
+  elasticsearch {
+    hosts => ["localhost:9200"]
+    sniffing => true
+    manage_template => false
+    index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
+    document_type => "%{[@metadata][type]}"
+  }
+}' | sudo tee /etc/logstash/conf.d/30-elasticsearch-output.conf
